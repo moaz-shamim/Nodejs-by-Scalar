7a797ed666877608189a9172922cb480f1a09b01
moaz-shamim <moaz.shamim1575@gmail.com>
2024-03-09 11:52:03 +0530

    Updating

--- 09How to Install and use an NPM Package/npmTest.js
+++ 09How to Install and use an NPM Package/npmTest.js
@@ -1,6 +1,6 @@
 const figlet = require("figlet");
 
-figlet("Hello I am Md Moaz Shamim!!", function (err, data) {
+figlet("Hello I am Moaz Shamim !", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
